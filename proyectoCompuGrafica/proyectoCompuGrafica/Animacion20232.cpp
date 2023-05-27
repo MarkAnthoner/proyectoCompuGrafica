4752d4f3fcef274f735dd9dc1f5bd31733960b6a
Pamela Castillo <pamela.castle@hotmail.com>
2023-05-27 17:13:16 -0600

    Modelo Capsulas

--- proyectoCompuGrafica/proyectoCompuGrafica/Animacion20232.cpp
+++ proyectoCompuGrafica/proyectoCompuGrafica/Animacion20232.cpp
@@ -162,6 +162,7 @@ Texture casaTexture;
 Texture LeviathanTexture;
 Texture PezRojoTexture;
 Texture PenglingTexture;
+Texture CapsulasTexture;
 
 // === Variables de Modelos ===
 
@@ -196,6 +197,8 @@ Model ArmLeftRobin;
 //Pengling
 Model Pengling_M;
 
+//Capsulas
+Model  Capsulas_M;
 
 Skybox skybox;	//skybox de día
 Skybox skybox_night; //skybox de noche
@@ -1403,6 +1406,8 @@ int main()
 	PezRojoTexture.LoadTextureA();
 	PenglingTexture = Texture("Textures/Pengling.png");
 	PenglingTexture.LoadTextureA();
+	CapsulasTexture = Texture("Textures/Capsulas.png");
+	CapsulasTexture.LoadTextureA();
 
 	// === Carga de modelos ===
 
@@ -1451,6 +1456,10 @@ int main()
 	//Pengling
 	Pengling_M = Model();
 	Pengling_M.LoadModel("Models/Pengling.obj");
+
+	//Capsulas
+	Capsulas_M = Model();
+	Capsulas_M.LoadModel("Models/Capsulas.obj");
 
 	// === Skybox ===
 	std::vector<std::string> skyboxFaces;
@@ -1774,7 +1783,6 @@ int main()
 		casaTexture.UseTexture();
 		meshList[12]->RenderMesh();
 		glDisable(GL_BLEND);
-
 
 
 		//=== Pengling: Modelo de pinguino ===
@@ -2218,6 +2226,17 @@ int main()
 		LeviathanTexture.UseTexture();
 		Material_opaco.UseMaterial(uniformSpecularIntensity, uniformShininess);
 		Leviathan_M.RenderModel();
+
+		//=== Capsulas ===
+		model = glm::mat4(1.0);
+		model = glm::translate(model, glm::vec3(50.0f, 0.0f, -10.0));
+		model = glm::scale(model, glm::vec3(10.0f, 10.0f, 10.0f));
+		model = glm::rotate(model, -90 * toRadians, glm::vec3(0.0f, 1.0f, 0.0f));
+		glUniformMatrix4fv(uniformModel, 1, GL_FALSE, glm::value_ptr(model));
+		Material_brillante.UseMaterial(uniformSpecularIntensity, uniformShininess);
+		CapsulasTexture.UseTexture();
+		Capsulas_M.RenderModel();
+
 
 		//=== Primitiva: Agua ===
 		model = glm::mat4(1.0);
